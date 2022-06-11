67f16882985261d90dba944a506c965c1cf78c98
panjikusumo7 <103624384+panjikusumo7@users.noreply.github.com>
2022-06-12 01:48:45 +0700

    Update renewtrgo.sh

--- trojango/renewtrgo.sh
+++ trojango/renewtrgo.sh
@@ -11,7 +11,6 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan-go/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
